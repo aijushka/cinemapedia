9520f9cda5fff076e8fa2f0c4cf16edf91c529f1
Aija Kuprina <aija.kuprina@helsinki.fi>
2026-02-26 17:38:26 +0200

    Using CSS & improving item list

--- items.py
+++ items.py
@@ -40,7 +40,10 @@ def get_classes(item_id):
     return db.query(sql, [item_id])
 
 def get_items():
-    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    sql = """SELECT items.id, items.title, items.rating, users.id user_id, users.username
+             FROM items, users
+             WHERE items.user_id = users.id
+             ORDER BY items.id DESC"""
     return db.query(sql)
 
 def get_item(item_id):
